f84c27e7b712c9625d1414451bab4a08b41c6851
Jack Lu <jacklu@jacklu.me>
2023-02-24 13:14:59 +0800

    persist plugins

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 /** @typedef { import("wechaty").Wechaty } Wechaty */
+var fs = require('fs');
+var path = require('path');
 var express = require('express');
 var importFresh = require('import-fresh');
 
 /**
  * @param {Object} config
  * @param {number} config.port web api port
+ * @param {string} config.dir working directory
  */
 module.exports = function WechatyDynamicPlugin(config) {
 	return function (/** @type {Wechaty} */bot) {
@@ -18,15 +21,40 @@ module.exports = function WechatyDynamicPlugin(config) {
 				installer: installer,
 				uninstaller: uninstaller
 			};
+			{
+				var file = path.join(config.dir, 'plugin.json');
+				let plugin = JSON.parse(fs.readFileSync(file, 'utf8'));
+				plugin[req.params.name] = req.body;
+				fs.writeFileSync(file, JSON.stringify(plugin, undefined, '\t'), 'utf8');
+			}
 			res.status(201).end();
 		});
 		app.delete('/plugin/:name', (req, res) => {
 			var uninstaller = plugin[req.params.name].uninstaller;
 			if (uninstaller) uninstaller();
 			delete plugin[req.params.name];
+			{
+				var file = path.join(config.dir, 'plugin.json');
+				let plugin = JSON.parse(fs.readFileSync(file, 'utf8'));
+				delete plugin[req.params.name];
+				fs.writeFileSync(file, JSON.stringify(plugin, undefined, '\t'), 'utf8');
+			}
 			res.status(204).end();
 		});
 		var server = app.listen(config.port);
+		var _plugin = plugin;
+		{
+			var file = path.join(config.dir, 'plugin.json');
+			let plugin = JSON.parse(fs.readFileSync(file, 'utf8'));
+			for (var name in plugin) {
+				var installer = importFresh(plugin[name].module);
+				var uninstaller = installer(bot);
+				_plugin[name] = {
+					installer: installer,
+					uninstaller: uninstaller
+				};
+			}
+		}
 		return () => {
 			for (var name in plugin)
 				if (plugin[name].uninstaller)
